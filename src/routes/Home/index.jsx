3003b6667ae870040fa89378c4b23c8ffd38ab03
Kamsi Kodi <kamsikodi@gmail.com>
2022-01-25 23:12:14 +0100

    Added hero image

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,11 +1,31 @@
-import { Box } from '@chakra-ui/react';
 import React from 'react';
+import { Box, Flex, Heading, HStack, Image, Stack, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import CarBg from '../../assets/images/cars_bg.jpg';
 import './style.scss';
 
 export default function Home() {
   return (
-    <Box h="100vh">
-      
+    <Box className="w__header" h="100vh">
+      <Flex w="100%" pos="absolute" color="white" px="4em" pt="2em" justifyContent="space-between">
+        <Heading className="w__title" fontSize="3rem">C-Rentals</Heading>
+        <HStack spacing={20}>
+          <Link to="/">
+            <Text fontSize="1.5rem" _hover={{ transform: 'translateY(-5px)', transition: 'all 300ms linear' }}>Shop</Text>
+          </Link>
+          <Link to="/">
+            <Text fontSize="1.5rem" _hover={{ transform: 'translateY(-5px)', transition: 'all 300ms linear' }}>Login</Text>
+          </Link>
+          <Link to="/">
+            <Text fontSize="1.5rem" _hover={{ transform: 'translateY(-5px)', transition: 'all 300ms linear' }}>Register</Text>
+          </Link>
+        </HStack>
+      </Flex>
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Stack>
+          <Heading color="white" className="w__hero-title">Welcome to C-Rentals, get your dream car now!</Heading>
+        </Stack>
+      </Box>
     </Box>
   );
 }
