7de61dc95fb36245727e807d84434b75edb5cda9
Kamsi Kodi <kamsikodi@gmail.com>
2022-01-25 22:09:27 +0100

    Project config

---
+++ src/routes/Home/index.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+import './style.scss';
+
+export default function Home() {
+  return (
+    <Box h="100vh">
+      
+    </Box>
+  );
+}
