48e6190b621eb697956aeee4a4f35690a76d01e4
Kamsi Kodi <kamsikodi@gmail.com>
2022-01-30 16:06:46 +0100

    Added Scroller

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Box, Heading, HStack, SimpleGrid, Stack, Text, Image } from '@chakra-ui/react';
+import { Link as ReactLink, animateScroll as scroll } from "react-scroll";
 import { Link } from 'react-router-dom';
 import './style.scss';
 import { gsap } from "gsap";
@@ -52,11 +53,11 @@ export default function Home() {
         <Stack className="w__hero-title" alignItems="center" textAlign="center" spacing="5em">
           <Heading fontSize="4rem" color="white" className="w__hero-title-text" data-test-id="header">Welcome to C-Rentals, get your dream car now!</Heading>
           <HStack spacing={10}>
-            <Link to="/">
+            <ReactLink to="car-listings" smooth={true} duration={500}>
               <Box py={5} px={20} border="1px solid white" borderRadius="30" className="c__hero-btn-left">
                 <Text color="white" fontWeight="bold">VIEW LISTINGS</Text>
               </Box>
-            </Link>
+            </ReactLink>
             <Link to="/">
               <Box py={5} px={20} bg="white" border="1px solid white" borderRadius="30" className="c__hero-btn-right">
                 <Text color="black" fontWeight="bold">FIND A CAR</Text>
@@ -66,15 +67,17 @@ export default function Home() {
         </Stack>
       </Box>
     </Box>
-    <Box>
+    <Box id="car-listings">
       <SimpleGrid columns={3} spacing={10} py="5em" px="5em">
         <CarCardItem />
         <CarCardItem />
         <CarCardItem />
-        <CarCardItem />
-        <CarCardItem />
-        <CarCardItem />
       </SimpleGrid>
+      <Link to="/">
+        <Box py={5} px={20} border="1px solid black" maxWidth="30%" textAlign="center" m="0 auto" mb="5em" className="c__hero-btn-left">
+          <Text color="black" fontWeight="bold">View More</Text>
+        </Box>
+      </Link>
     </Box>
     <Box bg="black">
       <HStack pt="2em" px="4em" justifyContent="space-between">
