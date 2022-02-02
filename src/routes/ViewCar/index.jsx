03ac718622cfb27838f987ba876fc6407b4facbf
Ben Gosling <goslingb@coventry.ac.uk>
2022-02-02 01:54:25 +0000

    Started working on layout for view car page

--- src/routes/ViewCar/index.jsx
+++ src/routes/ViewCar/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import { Text, Box, Flex, Image, HStack } from '@chakra-ui/react';
+import { Text, Box, Flex, Image, Stack, HStack, Spacer } from '@chakra-ui/react';
 import './style.scss';
 
 import Header from '../../components/Header';
@@ -16,24 +16,44 @@ export default function ViewCar() {
       <Header />
 
       <Flex align={'center'} justify={'center'}>
-        <Box w='20vw' h='92.5vh' bg='gray.200' />
-        <Box className="w__car" w='60vw' h='92.5vh' bg='white'>
+        <Box w='25vw' h='92.5vh' bg='gray.200' />
+        <Box className="w__car" w='50vw' h='92.5vh' bg='white'>
           <Link to="/">
             <Text className="w__car-link" _hover={{ textDecoration: 'underline', color: 'blue' }}>🠔 Return to homepage.</Text>
           </Link>
-          <HStack>
-            <Image maxH='500px' padding='25px' src={test_json.cars[0].image} alt={test_json.cars[0].model} />
-            <Flex h='300px'>
-              <Text className="w__car-title">{test_json.cars[0].make} {test_json.cars[0].model}</Text>
+
+          <HStack mt='25px'>
+            <Image className="w__image-large" ml='1vw' src={test_json.cars[0].image} alt={test_json.cars[0].model} />
+            <Flex h='400px' w='20vw' mr='1vw'>
+              <Stack w='20vw' align={'center'}>
+                <Spacer />
+                <Text className="w__car-title">{test_json.cars[0].make} {test_json.cars[0].model}</Text>
+                <Text className="w__car-subtitle">{test_json.cars[0].year}</Text>
+                <Spacer />
+                <Text className="w__car-title">£{test_json.cars[0].price}</Text>
+                <Spacer />
+              </Stack>
             </Flex>
-            
           </HStack>
-          <Text className="w__car-text">{test_json.cars[0].year}</Text>
-          <Text className="w__car-text">£{test_json.cars[0].price}</Text>
+
           <Text className="w__car-text">{test_json.cars[0].fuel}-{test_json.cars[0].mpg}mp/g</Text>
           <Text className="w__car-text">{test_json.cars[0].transmission}</Text>
         </Box>
-        <Box w='20vw' h='92.5vh' bg='gray.200' />
+
+        <Box w='25vw' h='92.5vh' bg='gray.200' justifyContent='center'>
+          <Flex ml='5vw' mr='5vw' mt='50px' justifyContent='center'>
+            
+            <Stack align='center' spacing='24px'>
+              <Text className="w__car-subtitle">View other similar cars</Text>
+              <Image className="w__image-small" src={test_json.cars[0].image} alt={test_json.cars[0].model} />
+              <Image className="w__image-small" src={test_json.cars[0].image} alt={test_json.cars[0].model} />
+              <Image className="w__image-small" src={test_json.cars[0].image} alt={test_json.cars[0].model} />
+              <Spacer />
+            </Stack>
+            
+          </Flex>
+        </Box>
+
       </Flex>
     </>
   );
